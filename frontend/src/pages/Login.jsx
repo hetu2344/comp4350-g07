c6fa4833d1b354b1e58228e4425282a35891fed7
Aswin Manoj <manoja@myumanitoba.ca>
2025-02-11 22:28:01 -0600

    Add pages to website: Added 3 pages (home, login and signup) pages to website

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import LogInPageNavigation from "../components/layout/LogInPageNavigation";
+import LogInForm from "../components/user-account/LogInForm";
+
+function LoginPage() {
+  return (
+    <div>
+      <LogInPageNavigation />
+      <LogInForm />
+    </div>
+  );
+}
+
+export default LoginPage;
