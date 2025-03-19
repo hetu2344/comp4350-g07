952e664afdaac6fad93ae34a12f5b3984813172d
Aswin Manoj <manoja@myumanitoba.ca>
2025-03-18 20:53:50 -0500

    Add cards for Dashboard page and css styling

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import React from "react";
 import Card from "react-bootstrap/Card";
 import menuImage from "../assets/menu.png";
+import sales from "../assets/growing.png";
 /* This page is the first page the staff sees when they log in */
 
 function DashboardPage({ user }) {
@@ -13,28 +14,51 @@ function DashboardPage({ user }) {
       <h1 className="text-3xl font-semibold text-gray-800 text-center mt-6">
         {user?.username || "Staff"}'s Dashboard
       </h1>
-
-      {/* Bootstrap Menu Management Card */}
-      <MenuManagementCard />
+      {/* Add appropriate cards below */}
+      <div className="dashboard-grid">
+        <NavigationCard
+          image={menuImage}
+          title="Menu Management"
+          navigateTo="/menu-management"
+        />
+        <NavigationCard
+          image={sales}
+          title="Sales Analysis"
+          navigateTo="/dashboard"
+        />
+      </div>
     </div>
   );
 }
 
-/* Clickable Bootstrap Card for Menu Management */
-const MenuManagementCard = () => {
+/* Reusable Navigation Card Component */
+const NavigationCard = ({ image, title, navigateTo }) => {
   const navigate = useNavigate();
 
   return (
     <Card
-      style={{ width: "18rem", cursor: "pointer" }}
+      style={{
+        width: "18rem",
+        cursor: "pointer",
+        textAlign: "center",
+        marginBottom: "20px",
+        padding: "10px" /* Add some padding inside the card */,
+        border: "1px solid #e2e8f0" /* Subtle border */,
+        borderRadius: "8px" /* Rounded corners */,
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)" /* Soft shadow */,
+        backgroundColor: "#fff" /* Add this line to set a white background */,
+      }}
       className="mt-5 shadow-lg"
-      onClick={() => navigate("/menu-management")}
+      onClick={() => navigate(navigateTo)}
     >
       <Card.Img
         variant="top"
-        src={menuImage}
-        style={{ width: "250px", height: "250px", objectFit: "cover" }}
+        src={image}
+        style={{ width: "250px", height: "250px", objectFit: "contain" }}
       />
+      <Card.Body>
+        <Card.Text className="card-title">{title}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
