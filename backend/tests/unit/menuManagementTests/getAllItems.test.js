c3be2df8252f604731469ff5ddbed70d7e5e2826
Divy Patel <divyp630@gmail.com>
2025-03-20 14:08:45 -0500

    Added some more test to reach 100%

---
+++ backend/tests/unit/menuManagementTests/getAllItems.test.js
@@ -0,0 +1,114 @@
+const request = require("supertest");
+const app = require("../../../index");
+const pool = require("../../../db/db");
+
+// Creating a mock database connection
+jest.mock("../../../db/db", () => {
+  const mockClient = {
+    query: jest.fn(),
+    release: jest.fn(),
+  };
+
+  return {
+    query: jest.fn(),
+    connect: jest.fn().mockResolvedValue(mockClient),
+    end: jest.fn().mockResolvedValue(),
+    __mockClient: mockClient,
+  };
+});
+
+// Setting things to do after each test
+describe("Menu Mannagement GET ALL API Tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  // Test that gets all menu items
+  test("should return all menu items along with category name and a list of allergens", async () => {
+    const expectedItem = [
+      {
+        item_id: 1,
+        item_name: "Margherita Pizza",
+        item_description: "Classic pizza with mozzarella, tomatoes, and basil.",
+        price: "12.99",
+        category_id: 2,
+        is_available: true,
+        is_vegetarian: true,
+        is_vegan: false,
+        is_gluten_free: false,
+        created_at: "2025-02-21T23:21:46.158Z",
+        category_name: "Main Course",
+        allergens: [],
+      },
+      {
+        item_id: 2,
+        item_name: "Vegan Buddha Bowl",
+        item_description: "Quinoa, chickpeas, avocado, and tahini dressing.",
+        price: "11.99",
+        category_id: 2,
+        is_available: true,
+        is_vegetarian: false,
+        is_vegan: true,
+        is_gluten_free: true,
+        created_at: "2025-02-21T23:21:46.160Z",
+        category_name: "Main Course",
+        allergens: [],
+      },
+      {
+        item_id: 3,
+        item_name: "Cheesecake",
+        item_description:
+          "Rich and creamy cheesecake topped with strawberries.",
+        price: "6.49",
+        category_id: 3,
+        is_available: true,
+        is_vegetarian: true,
+        is_vegan: false,
+        is_gluten_free: false,
+        created_at: "2025-02-21T23:21:46.161Z",
+        category_name: "Dessert",
+        allergens: [],
+      },
+    ];
+
+    pool.query.mockResolvedValue({ rows: expectedItem });
+
+    const res = await request(app).get("/api/menu/");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual(expectedItem);
+    expect(pool.query).toHaveBeenCalledTimes(1);
+  });
+
+  // Test that tries to get all menu items and returns Not found(404)
+  test("should fail to return menu item with a specific id and return 404", async () => {
+    pool.query.mockResolvedValue({ rows: [] });
+
+    const res = await request(app).get("/api/menu/");
+
+    const errorMsg = {
+      error: "No menu items found.",
+    };
+
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual(errorMsg);
+  });
+
+  // Test that gets all  menu items
+  test("should fail to return all menu items and return 500", async () => {
+    pool.query.mockRejectedValue(new Error("Database Connection Failed"));
+
+    const res = await request(app).get("/api/menu/");
+
+    const errorMsg = {
+      error: "Server Error: Unable to fetch menu item.",
+    };
+
+    expect(res.statusCode).toBe(500);
+    expect(res.body).toEqual(errorMsg);
+  });
+});
---
+++ backend/tests/unit/menuManagementTests/insertInMenu.test.js
@@ -0,0 +1,196 @@
+const request = require("supertest");
+const app = require("../../../index");
+const pool = require("../../../db/db");
+
+// Creating a mock database connection
+jest.mock("../../../db/db", () => {
+  const mockClient = {
+    query: jest.fn(),
+    release: jest.fn(),
+  };
+
+  return {
+    query: jest.fn(),
+    connect: jest.fn().mockResolvedValue(mockClient),
+    end: jest.fn().mockResolvedValue(),
+    __mockClient: mockClient,
+  };
+});
+
+// Setting things to do after each test
+describe("Menu Mannagement API Tests", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  // Test that inserts an item in the menu
+  test("should insert a new item in the menu", async () => {
+    pool.__mockClient.query.mockImplementation((sql, params) => {
+      if (sql.includes("SELECT id FROM menu_categories")) {
+        return Promise.resolve({ rows: [{ id: 2 }] });
+      }
+      if (sql.includes("INSERT INTO menu_items")) {
+        return Promise.resolve({
+          rows: [{ item_id: 4, item_name: "Veggie Burger" }],
+        });
+      }
+      return Promise.resolve({ rows: [] });
+    });
+
+    const newMenuItem = {
+      itemName: "Veggie Burger",
+      itemDescription: "Delicious plant-based burger",
+      price: 12.99,
+      category: "Main Course",
+      isAvailable: true,
+      isVegetarian: false,
+      isVegan: false,
+      isGlutenFree: false,
+      allergens: ["Dairy"],
+    };
+
+        const expectedMessage = {
+          message: "Veggie Burger added successfully.",
+          item_id: 4,
+        };
+
+    const res = await request(app)
+      .post("/api/menu")
+      .send(newMenuItem)
+      .set("Accept", "application/json");
+    console.log("Actual response:", res.body);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toEqual(expectedMessage);
+    expect(pool.__mockClient.query).toHaveBeenCalledTimes(7);
+
+
+  });
+
+
+    // Test that inserts an item in the menu
+  test("should try to insert a new item in the menu but fail due to category not fund", async () => {
+    pool.__mockClient.query.mockImplementation((sql, params) => {
+      if (sql.includes("SELECT id FROM menu_categories")) {
+        return Promise.resolve({ rows: [] });
+      }
+      return Promise.resolve({ rows: [] }); 
+    });
+
+    const expectedMessage = {
+    "error": "Category not found"
+    }
+
+    const newMenuItem = {
+      itemName: "Veggie Burger",
+      itemDescription: "Delicious plant-based burger",
+      price: 12.99,
+      category: "Diet",
+      isAvailable: true,
+      isVegetarian: false,
+      isVegan: false,
+      isGlutenFree: false,
+      allergens: ["Dairy"],
+    };
+
+    const res = await request(app)
+      .post("/api/menu")
+      .send(newMenuItem)
+      .set("Accept", "application/json");
+    console.log("Actual response:", res.body);
+
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual(expectedMessage);
+    expect(pool.__mockClient.query).toHaveBeenCalledTimes(3);
+  });
+
+  // Test that fails to insert an item in the menu
+  test("should fail to insert a new item in the menu", async () => {
+    pool.__mockClient.query.mockImplementation((sql, params) => {
+      if (sql.includes("SELECT id FROM menu_categories")) {
+        return Promise.resolve({ rows: [{ id: 2 }] });
+      }
+      if (sql.includes("INSERT INTO menu_items")) {
+        return Promise.resolve({
+          rows: [],
+        });
+      }
+      return Promise.resolve({ rows: [] });
+    });
+
+    const expectedMessage = {
+      error: "Server Error : Unable to add item to the menu.",
+    };
+
+    const newMenuItem = {
+      itemName: "Veggie Burger",
+      itemDescription: "Delicious plant-based burger",
+      price: 12.99,
+      category: "Main Course",
+      isAvailable: true,
+      isVegetarian: false,
+      isVegan: false,
+      isGlutenFree: false,
+      allergens: [],
+    };
+
+
+    const res = await request(app)
+      .post("/api/menu")
+      .send(newMenuItem)
+      .set("Accept", "application/json");
+    console.log("Actual response:", res.body);
+
+    expect(res.statusCode).toBe(500);
+    expect(res.body).toEqual(expectedMessage);
+    expect(pool.__mockClient.query).toHaveBeenCalledTimes(5);
+  });
+
+
+  test("should fail to insert a new item in the menu", async () => {
+    pool.__mockClient.query.mockImplementation((sql, params) => {
+      if (sql.includes("SELECT id FROM menu_categories")) {
+        return Promise.resolve({ rows: [{ id: 2 }] });
+      }
+      if (
+        sql.includes(
+          "INSERT INTO menu_items (item_name,item_description,price,category_id,is_available,is_vegetarian,is_vegan,is_gluten_free) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING item_id,item_name"
+        )
+      ) {
+        return Promise.reject(new Error("Database Error"));
+      }
+      return Promise.resolve({ rows: [] });
+    });
+
+    const newMenuItem = {
+      itemName: "Veggie Burger",
+      itemDescription: "Delicious plant-based burger",
+      price: 12.99,
+      category: "Diet",
+      isAvailable: true,
+      isVegetarian: false,
+      isVegan: false,
+      isGlutenFree: false,
+      allergens: ["Dairy"],
+    };
+
+    const expectedMessage = {
+      error: "Server Error : Unable to add item to the menu.",
+    };
+
+    const res = await request(app)
+      .post("/api/menu")
+      .send(newMenuItem)
+      .set("Accept", "application/json");
+    console.log("Actual response:", res.body);
+
+    expect(res.statusCode).toBe(500);
+    expect(res.body).toEqual(expectedMessage);
+  });
+
+});
+
