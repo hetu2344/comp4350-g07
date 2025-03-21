e43aea661cf4a53b7d33eecc8bb9524c265e2d76
hetu <patelh29@myumanitoba.ca>
2025-03-21 04:19:03 -0500

    fixed the bug related to testing

---
+++ backend/db/test_db.js
@@ -0,0 +1,16 @@
+const Pool = require("pg").Pool;
+const types = require("pg").types;
+// Returns DECIMAL as Number instead of String
+types.setTypeParser(1700, (val) => parseFloat(val)); 
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.TEST_DB_HOST,
+    database: process.env.TEST_DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.TEST_DB_PORT,
+});
+
+
+module.exports = pool;
+
