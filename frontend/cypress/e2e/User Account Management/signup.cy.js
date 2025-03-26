d1931ec481990bb2c9fc70514d2f8470ed024cf3
Aswin Manoj <manoja@myumanitoba.ca>
2025-03-26 13:57:26 -0500

    Add LogOut acceptance tests for User Account Management

--- frontend/cypress/e2e/User Account Management/signup.cy.js
+++ frontend/cypress/e2e/User Account Management/signup.cy.js
@@ -1,4 +1,4 @@
-describe('Create Staff Account tests', () => {
+describe('Create Staff Account', () => {
 
     beforeEach(() => {
         //Visit the frontend on port 8017
@@ -40,7 +40,7 @@ describe('Create Staff Account tests', () => {
         cy.url().should('include', '/dashboard');
     });
   
-    it('should show an error if the sign-up fails', () => {
+    it('should show an error if the sign-up fails because user already exists', () => {
         
      //Wait for the API request to be intercepted
         cy.intercept('POST', 'http://localhost:8018/api/user/signup').as('signUpRequest');
@@ -48,14 +48,14 @@ describe('Create Staff Account tests', () => {
       //Fill out the form with invalid data
         cy.get('input#fname').type('Owner');
         cy.get('input#lname').type('John');
-        cy.get('input#userID').type('owner_john'); // Use an existing username
+        cy.get('input#userID').type('owner_john');
         cy.get('input#password').type('password123');
         cy.get('select#role').select('S');
   
       //Submit the form
         cy.get('button').contains('SIGN UP').click();
 
-        cy.wait('@signUpRequest').its('response.statusCode').should('eq', 409); // Conflict error for existing user
+        cy.wait('@signUpRequest').its('response.statusCode').should('eq', 409); //Conflict error for existing user
   
       //Verify the error message is displayed
         cy.contains('User with username \'owner_john\' already exists.');
